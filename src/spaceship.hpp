eb699ad3a91fd0b6039ef57b598d3cca6d6f4305
Jatin-Madhikarmi <jatinmadhikarmi8@gmail.com>
2024-07-27 09:31:34 +0545

    Your commit message

--- src/spaceship.hpp
+++ src/spaceship.hpp
@@ -1,7 +1,21 @@
 #pragma once
+#include<raylib.h>
 
 class SpaceShip
 {
-    
+    SpaceShip();
+    ~SpaceShip();
+    Rectangle rect;
+    int speed;
+    bool active;
+    Color color;
+    Texture2D texture;
+    Rectangle frameRec;
+    Rectangle hitbox;
+    int currentFrame;
+    int frameCounter;
+    int frameSpeed;
+    void draw();
+    void update();
 
 };
