cbd68964df8a3d323e070ad96fe7fb8fda4e26d5
Jatin-Madhikarmi <jatinmadhikarmi8@gmail.com>
2024-08-16 01:04:44 +0545

    Bullet is sucessfully animated in the tutorial screen

--- src/tutorial1_control.cpp
+++ src/tutorial1_control.cpp
@@ -87,104 +87,6 @@ void TutorialPlayer::update1()
     }
 }
 
-// void TutorialPlayer::update2()
-// {
-//     if (!Tut2Done)
-//     {
-//         DrawText("MOVE THE RIGHT ARROW KEY TO MOVE RIGHT", 0.0f, 0.0f, 50, WHITE);
-
-//         if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT))
-//         {
-//             if (!rightarrowpressed)
-//             {
-//                 pressedtime2 = GetTime();
-//                 std::cout << "Right arrow pressed at time: " << pressedtime2 << std::endl;
-//                 rightarrowpressed = true;
-//             }
-//             FinalRectangle.x += speed;
-//             if (GetTime() - pressedtime2 >= 1.0f)
-//             {
-//                 Tut2Done = true;
-//                 finishes = GetTime();
-//                 std::cout << "Tutorial 2 completed at time: " << finishes << std::endl;
-//             }
-//         }
-//     }
-//     else
-//     {
-//         if (!congratulationsfinished)
-//         {
-//             DrawText("CONGRATULATIONS", 550, 200, 50, WHITE);
-//             std::cout << "Displaying congratulations in update2" << std::endl;
-//         }
-
-//         if ((GetTime() - finishes >= 1.0f))
-//         {
-//             congratulationsfinished = true;
-//             std::cout << "Calling update3 from update2 at time: " << GetTime() << std::endl;
-//             update3(); // Call to update3
-//         }
-//     }
-// }
-
-// void TutorialPlayer::update3()
-// {
-    
-//     if(!Tut3Done)
-//     {
-//         DrawText("PRESS THE SPACE KEY TO SHOOT BULLETS",0.0f,0.0f,50,WHITE);
-
-//         if(IsKeyDown(KEY_ENTER))
-//         {
-//             if(!enterkeypressed)
-//         {
-//             pressedtime3=GetTime();
-//             std::cout<<"The value of enterkeypressed is "<<enterkeypressed<<std::endl;
-//             enterkeypressed=true;
-//         }
-//         Bullet newBullet;
-//             newBullet.rect = { FinalRectangle.x + FinalRectangle.width / 2 , FinalRectangle.y,(float) B_image.width,(float) B_image.height };
-//             // newBullet.speed = { 0,  };
-//             newBullet.active = true;
-//             bullets.push_back(newBullet);
-//         }
-//         for (auto& bullet : bullets) 
-//         {
-//             if (bullet.active) 
-//             {
-//                 bullet.rect.y += 5;
-//                 if (bullet.rect.y < -B_image.height) 
-//                 {
-//                     bullet.active = false;
-//                 }
-//             }
-//         }
-//          bullets.erase(
-//             std::remove_if(bullets.begin(), bullets.end(), [](const Bullet& b) { return !b.active; }),
-//             bullets.end()
-//         );
-//         //DrawRectangle(playerPosition.x, playerPosition.y, playerWidth, playerHeight, BLUE);
-//         for (const auto& bullet : bullets) {
-//             if (bullet.active) {
-//                 DrawRectangleRec(bullet.rect, RED);
-//             }
-//         }
-
-//         if(GetTime()-pressedtime3>=1.0f)
-//         {
-//             Tut3Done=true;
-//             std::cout<<"The tutorial 2 of bullet firing is completed\n";
-//         }
-//         std::cout<<"The value of pressed time is "<<pressedtime3<<std::endl;
-//         std::cout<<"The value of gettime is "<<GetTime()<<std::endl;
-//         std::cout<<"The tutorial 1 of movement is not completed\n";
-//     }
-//     else
-//     {
-//         DrawText("CONGRATULATIONS",550,200,50,WHITE);
-//     }
-// }
-
 
 void TutorialPlayer::update2()
 {
@@ -230,30 +132,7 @@ void TutorialPlayer::update2()
         }
     }
 }
-// void TutorialPlayer::update3()
-// {
-//     if(!Tut3Done)
-//     {
-//         DrawText("PRESS SPACE TO SHOOT",0.0f,0.0f,50,WHITE);
 
-//         if(IsKeyDown(KEY_SPACE))
-//     {
-        
-//         if(GetTime()-pressedtime2>=1.0f)
-//         {
-//             Tut3Done=true;
-//             std::cout<<"The tutorial 3 of bullet is completed\n";
-//         }
-//         std::cout<<"The value of pressed time is "<<pressedtime2<<std::endl;
-//         std::cout<<"The value of gettime is "<<GetTime()<<std::endl;
-//         std::cout<<"The tutorial 1 of movement is not completed\n";
-//     }
-//     }
-//     else
-//     {
-//         DrawText("CONGRATULATIONS",550,200,50,WHITE);
-//     }
-// }
 
 void TutorialPlayer::update3()
 {
@@ -263,7 +142,7 @@ void TutorialPlayer::update3()
     {
         DrawText("PRESS THE SPACE KEY TO SHOOT BULLETS", 0.0f, 0.0f, 50, WHITE);
 
-        if (IsKeyDown(KEY_SPACE))
+        if (IsKeyPressed(KEY_SPACE))
         {
             if (!enterkeypressed)
             {
